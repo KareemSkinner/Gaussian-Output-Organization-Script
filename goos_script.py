0214685b76ead7e7deffb3757a9cf0aaf8ffbde2
Kareem Skinner <kastsk@gmail.com>
2019-08-12 17:07:45 -0400

    * completed script * script now reads all files in its folder and outputs filename, physical parameters and sum of the electronic and thermal free energies to an output file

--- goos_script.py
+++ goos_script.py
@@ -1,23 +1,33 @@
-import os, re
+# this script reads Gaussian .log files located in its folder. Writes the file name, physical parameters, and the Sum of electronic and thermal Free Energies to an output file
+# makes getting this data through Gaussian .log files a lot quicker
 
-input_file = open("acetaldehyde.log", "r")
-input_file_name = os.path.basename(input_file.name)
-
-datastring = "Sum of electronic and thermal Free Energies".lower()
+import os # for working with filenames 
+import re # regex library
 
 output_file = open("python_script_output.txt", "a+")
 
-file = input_file.readlines()
+print("Working on files...")
 
-print("Working on file...")
+currentDirectory = os.getcwd()  # gets present working directory
+for filename in os.listdir(currentDirectory):
+        try:
+            if filename.endswith(".log"):
+                input_file = open(filename, "r")
+                print(filename, file = output_file)
+                current_file = input_file.readlines()
 
-print(input_file_name, file = output_file)
-for input_line in file:
-    if input_line.lower().find("temperature" and "pressure" and "connectivity") != -1:
-        print(input_line, file = output_file)
-    elif input_line.lower().find(datastring) != -1:
-        print(input_line, file = output_file)
-print("File completed")
+                for input_line in current_file:
+                    datastring = "Sum of electronic and thermal Free Energies".lower()
+
+                    if input_line.lower().find("temperature" and "kelvin" and "pressure") != -1:
+                        print(input_line, file = output_file)
+                    if input_line.lower().find(datastring) != -1:
+                        print(input_line, file = output_file)
+        except Exception as noFile:
+            raise noFile
+            print ("No files found here...")
+
+print("Data retrieval complete.")
 
 input_file.close()
 output_file.close()
