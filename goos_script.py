ffae8c0860cc86cc50b662fd098495afce9eab6f
Kareem Skinner <kastsk@gmail.com>
2019-08-12 12:38:49 -0400

    * added ability to search through file and extract data

--- goos_script.py
+++ goos_script.py
@@ -1,8 +1,25 @@
+import os, re
 
-import re
+input_file = open("acetaldehyde.log", "r")
+input_file_name = os.path.basename(input_file.name)
 
-input_file = open("acetaldehyde.log")
+datastring = "Sum of electronic and thermal Free Energies".lower()
+
+output_file = open("python_script_output.txt", "a+")
 
 file = input_file.readlines()
-for x in file:
-    print(x)
+
+print("Working on file...")
+
+print(input_file_name, file = output_file)
+for input_line in file:
+    if input_line.lower().find("temperature" and "pressure" and "connectivity") != -1:
+        print(input_line, file = output_file)
+    elif input_line.lower().find(datastring) != -1:
+        print(input_line, file = output_file)
+print("File completed")
+
+input_file.close()
+output_file.close()
+
+input("Press Enter to exit...")
