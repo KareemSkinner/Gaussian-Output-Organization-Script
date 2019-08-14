f9f09abb4977b35bbffa4ec0c065586157c083da
Kareem Skinner <kastsk@gmail.com>
2019-08-14 16:30:47 -0400

    * bug fixes  - opened files weren't being closed. - created parameter string for easy search modification.

--- goos_script.py
+++ goos_script.py
@@ -1,35 +1,33 @@
-# this script reads Gaussian .log files located in its folder. Writes the file name, physical parameters, and the Sum of electronic and thermal Free Energies to an output file
-# makes getting this data through Gaussian .log files a lot quicker
+""" This script reads Gaussian .log files located in its folder. 
+    Writes the file name, physical parameters, and the Sum of electronic and thermal Free Energies to an output file
+    Makes getting this data through Gaussian .log files a lot quicker
+""" 
 
-import os # for working with filenames 
-import re # regex library
+import os 
+import re 
 
 output_file = open("python_script_output.txt", "a+")
-
 print("Working on files...")
+currentDirectory = os.getcwd()      # gets present working directory
 
-currentDirectory = os.getcwd()  # gets present working directory
-for filename in os.listdir(currentDirectory):
+for filename in os.listdir(currentDirectory):   # loops through each file in current directory
+    if filename.endswith(".log"):
         try:
-            if filename.endswith(".log"):
-                input_file = open(filename, "r")
+            with open(filename, "r") as input_file:     # handles closing of every input file opened
                 print(filename, file = output_file)
+                print("----------------------------------------------------------------------------------------", file = output_file) # separates outputs of each file
                 current_file = input_file.readlines()
-
-                for input_line in current_file:
-                    datastring = "Sum of electronic and thermal Free Energies".lower()
-
-                    if input_line.lower().find("temperature" and "kelvin" and "pressure") != -1:
+                parameterstring = "temperature="                                        # extracts parameters used in Gaussian log
+                datastring = "Sum of electronic and thermal Free Energies".lower()      # extracts specific line of desired data in Gaussian log 
+                for input_line in current_file:     # loops through each line in current file
+                    if input_line.lower().find(parameterstring) != -1:
                         print(input_line, file = output_file)
                     if input_line.lower().find(datastring) != -1:
                         print(input_line, file = output_file)
-        except Exception as noFile:
-            raise noFile
+        except IOError as noFile:
             print ("No files found here...")
-
+            raise noFile       
 print("Data retrieval complete.")
-
-input_file.close()
 output_file.close()
 
 input("Press Enter to exit...")
